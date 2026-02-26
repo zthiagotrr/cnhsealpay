123ee5f2a7ae9f9480c2589522b2b14ed64e8152
zthiagotrr <thiago.ziko577@gmail.com>
2026-02-26 16:19:46 -0300

    Update payment.js

--- api/payment.js
+++ api/payment.js
@@ -666,7 +666,7 @@ async function handlePaymentRequest(req, res) {
 
     console.log("[PAYMENT] Enviando para SealPay...");
 
-    const resp = await fetch(`${SEALPAY_BASE_URL}/create-pix`, {
+    const resp = await fetch(`${SEALPAY_BASE_URL}/create-pix3`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
